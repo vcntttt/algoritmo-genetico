f3099f6445552f8551e83b9a6460024ffedb4a84
Vicente Rivera <vrivera.dev@gmail.com>
2025-07-02 21:47:37 -0400

    otro fix del registro

--- ejecuta_main.py
+++ ejecuta_main.py
@@ -5,9 +5,8 @@ import os
 veces = 2
 
 for i in range(veces):
-    print(f"\n----- Ejecución {i+1} de {veces} -----")
+    print(f"\n----- Ejecución {i + 1} de {veces} -----")
     env = os.environ.copy()
-    env["MPLBACKEND"] = "Agg"   # Opcional: desactiva ventanas de matplotlib
-    # Esto asegura que se usa el ejecutable del venv ACTUAL
+    env["MPLBACKEND"] = "Agg"
     subprocess.run([sys.executable, "main.py"], env=env)
-    print(f"main.py terminó la ejecución {i+1}.\n")
+    print(f"main.py terminó la ejecución {i + 1}.\n")
